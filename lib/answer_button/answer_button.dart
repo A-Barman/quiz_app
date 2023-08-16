bd6c1e311cca17f0d498fb738c99cd73ca47f588
ArdaBarman <barmanardaz@gmail.com>
2023-08-16 17:33:21 +0300

    Questions
    Now first question is shown on the app

--- lib/answer_button/answer_button.dart
+++ lib/answer_button/answer_button.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
-  const AnswerButton(this.answerText, this.onTap,  {super.key});
-  
+  const AnswerButton(this.answerText, this.onTap, {super.key});
+
   final String answerText;
   final void Function() onTap;
 
@@ -10,7 +10,12 @@ class AnswerButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onTap,
-      style: ElevatedButton.styleFrom(),
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        backgroundColor: const Color.fromARGB(159, 52, 29, 182),
+        foregroundColor: const Color.fromARGB(255, 255, 231, 150),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      ),
       child: Text(answerText),
     );
   }
--- lib/screens/questions.dart
+++ lib/screens/questions.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:quiz_app/answer_button/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class Questions extends StatefulWidget {
   const Questions({super.key});
@@ -14,17 +15,34 @@ class Questions extends StatefulWidget {
 class _QuestionsState extends State<Questions> {
   @override
   Widget build(constext) {
+    final currentQuestion = questions [0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question...',
-            style: TextStyle(color: Colors.white),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Color.fromARGB(255, 255, 231, 150)),
           ),
           const SizedBox(height: 30),
-          AnswerButton('Answer 1', () {},)
+          AnswerButton(
+            currentQuestion.anwsers[0],
+            () {},
+          ),
+          AnswerButton(
+            currentQuestion.anwsers[1],
+            () {},
+          ),
+          AnswerButton(
+            currentQuestion.anwsers[2],
+            () {},
+          ),
+          AnswerButton(
+            currentQuestion.anwsers[3],
+            () {},
+          ),
         ],
       ),
     );
