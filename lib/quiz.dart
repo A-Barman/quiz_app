65fa5a09a000a939a063baee26237ab5c3107575
ArdaBarman <barmanardaz@gmail.com>
2023-08-18 17:50:15 +0300

    Error
    Couldn't fix the error yet

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -5,46 +5,48 @@ import 'package:quiz_app/screens/results_screen.dart';
 import 'package:quiz_app/screens/start_screen.dart';
 import 'package:quiz_app/styled_widgets/gradient_container.dart';
 
-class Quiz extends StatefulWidget{
+class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
   @override
-  State<Quiz> createState(){
+  State<Quiz> createState() {
     return _QuizState();
   }
 }
 
 class _QuizState extends State<Quiz> {
-List<String> selectedAnswers = [];
-late Widget activeScreen;
+  List<String> selectedAnswers = [];
+  late Widget activeScreen;
 
-
-@override
+  @override
   void initState() {
     super.initState();
     activeScreen = StartScreen(switchScreen);
   }
 
-void switchScreen() {
-  setState(() {
-    activeScreen = Questions(onSelectAnswer: chooseAnswer,);
-  });
-}
-
-void chooseAnswer(String answer) {
-  selectedAnswers.add(answer);
-
-  if (selectedAnswers.length == questions.length){
+  void switchScreen() {
     setState(() {
-      selectedAnswers = [];
-      activeScreen = const ResultScreen();
+      activeScreen = Questions(
+        onSelectAnswer: chooseAnswer,
+      );
     });
   }
-}
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = ResultScreen(
+          chosenAnswer: selectedAnswers,
+        );
+      });
+    }
+  }
 
   @override
   Widget build(context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Stack(
           children: [
--- lib/screens/results_screen.dart
+++ lib/screens/results_screen.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary/questions_summary.dart';
+import 'package:quiz_app/styled_widgets/styled_text.dart';
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen({super.key});
+  const ResultScreen({
+    super.key,
+    required this.chosenAnswer,
+  });
+
+  final List<String> chosenAnswer;
+
+  List<Map<String, Object>> getSumaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswer.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswer[i]
+      });
+    }
+
+    return summary;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -11,13 +35,17 @@ class ResultScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You answered X out of Y questions corretly !'),
+            const StyledText('You answered X out of Y questions corretly !'),
             const SizedBox(
-              height: 30,
+              height: 20,
             ),
-            const Text('data'),
+            SizedBox(
+              height: 500,
+              width: 340,
+              child: QuestionsSummary(getSumaryData()),
+            ),
             const SizedBox(
-              height: 30,
+              height: 20,
             ),
             TextButton(
               onPressed: () {},
