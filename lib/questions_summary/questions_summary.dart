61efaff2e0208bf801f56bd5c8bf0c4e37ba048d
ArdaBarman <barmanardaz@gmail.com>
2023-08-19 22:01:01 +0300

    typo_fixed
    The reason of the error is seems like a typo and now it is fixed

--- lib/questions_summary/questions_summary.dart
+++ lib/questions_summary/questions_summary.dart
@@ -21,7 +21,7 @@ class QuestionsSummary extends StatelessWidget {
                   const SizedBox(height: 5,),
                   Text(data['user_answer'] as String),
                   const SizedBox(height: 5,),
-                  Text(data['chosenAnswer'] as String),
+                  Text(data['correct_answer'] as String),
                 ],
               ),
             ],
